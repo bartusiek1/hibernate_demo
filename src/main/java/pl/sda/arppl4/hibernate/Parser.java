2686784fe98f72d9650e2b24c8c8fb587ae214fe
Your Name <grubabartek@gmail.com>
2022-07-01 11:24:48 +0200

    parser poprawiony, ale edycja nie do końca ok

--- src/main/java/pl/sda/arppl4/hibernate/Parser.java
+++ src/main/java/pl/sda/arppl4/hibernate/Parser.java
@@ -1,7 +1,6 @@
 package pl.sda.arppl4.hibernate;
 
 
-
 import pl.sda.arppl4.hibernate.dao.StudentDao;
 import pl.sda.arppl4.hibernate.model.Student;
 
@@ -34,74 +33,74 @@ public class Parser {
         System.out.println("Podaj numer id Studenta");
         Long podaneId = scanner.nextLong();
         Optional<Student> szukanyStudent = studentDao.zwrocStudenta(podaneId);
-        if (szukanyStudent.isPresent()) {
-            Student wybranyStudent = szukanyStudent.get();
-            System.out.println("Wskaż parametr do zmiany - Imie/Nazwisko");
-            String wybranyParametr = scanner.nextLine();
 
-            if (wybranyParametr.equalsIgnoreCase("Imie")) {
-                System.out.println("Wskaż imie po modyfikacji");
-                String noweImie = scanner.nextLine();
-                wybranyStudent.setName(noweImie);
-                System.out.println("Imie studenta " + podaneId + " zostało zmienione na: " + noweImie);
-            } else if (wybranyParametr.equalsIgnoreCase("nazwisko")) {
-                System.out.println("Wskaż nazwisko po modyfikacji");
-                String noweNazwisko = scanner.nextLine();
-                wybranyStudent.setSurname(noweNazwisko);
-                System.out.println("Nazwisko studenta " + podaneId + " zostało zmienione na: " + noweNazwisko);
-            }
+        if (szukanyStudent.isPresent()) {
+           Student wybranyStudent = szukanyStudent.get();
+            System.out.println("Wskaż imie po modyfikacji");
+            String noweImie = scanner.next();
+            wybranyStudent.setName(noweImie);
+            System.out.println("Imie studenta " + podaneId + " zostało zmienione na: " + noweImie);
+            System.out.println("Wskaż nazwisko po modyfikacji");
+            String noweNazwisko = scanner.next();
+            wybranyStudent.setSurname(noweNazwisko);
+            System.out.println("Nazwisko studenta " + podaneId + " zostało zmienione na: " + noweNazwisko);
+            System.out.println("Wskaż nr indeksu po modyfikacji");
+            String nowyIndeks = scanner.next();
+            System.out.println("Indeks studenta " + podaneId + " został zmieniony na: " + nowyIndeks);
+            wybranyStudent.setIndexNumber(nowyIndeks);
+
             studentDao.updateStudent(wybranyStudent);
         }
     }
 
-        private void obsluzLista () {
-            List<Student> listaStudentow = studentDao.zwrocListeStudentow();
-            for (Student student : listaStudentow) {
-                System.out.println(student);
-            }
-        }
-
-        private void obsluzUsun () {
-            System.out.println("Podaj numer id Studenta");
-            Long podaneId = scanner.nextLong();
-            Optional<Student> szukanyStudent = studentDao.zwrocStudenta(podaneId);
-            if (szukanyStudent.isPresent()) {
-                Student student = szukanyStudent.get();
-                studentDao.usunStudenta(student);
-            }
-        }
-
-        private void obsluzDodaj () {
-            System.out.println("Podaj imię:");
-            String imie = scanner.nextLine();
-            System.out.println("Podaj nazwisko:");
-            String nazwisko = scanner.nextLine();
-            System.out.println("Podaj numer indeksu");
-            String indeks = scanner.nextLine();
-            System.out.println("Podaj datę urodzenia");
-            String data = scanner.nextLine();
-
-            Student student = new Student(null, imie, nazwisko, indeks, data);
-            studentDao.dodajStudenta(student);
-        }
-
-        public void dzialaj () {
-            String tekst;
-            do {
-                System.out.println("Wybierz komendę: dodaj/usun/lista/edytuj");
-                tekst = scanner.nextLine();
-
-
-                if (tekst.equalsIgnoreCase("dodaj")) {
-                    obsluzDodaj();
-                } else if (tekst.equalsIgnoreCase("usun")) {
-                    obsluzUsun();
-                } else if (tekst.equalsIgnoreCase("lista")) {
-                    obsluzLista();
-                } else if (tekst.equalsIgnoreCase("edytuj")) {
-                    obsluzEdytuj();
-                }
-            } while (!tekst.equalsIgnoreCase("quit"));
+    private void obsluzLista() {
+        List<Student> listaStudentow = studentDao.zwrocListeStudentow();
+        for (Student student : listaStudentow) {
+            System.out.println(student);
         }
     }
+
+    private void obsluzUsun() {
+        System.out.println("Podaj numer id Studenta");
+        Long podaneId = scanner.nextLong();
+        Optional<Student> szukanyStudent = studentDao.zwrocStudenta(podaneId);
+        if (szukanyStudent.isPresent()) {
+            Student student = szukanyStudent.get();
+            studentDao.usunStudenta(student);
+        }
+    }
+
+    private void obsluzDodaj() {
+        System.out.println("Podaj imię:");
+        String imie = scanner.next();
+        System.out.println("Podaj nazwisko:");
+        String nazwisko = scanner.next();
+        System.out.println("Podaj numer indeksu");
+        String indeks = scanner.next();
+        System.out.println("Podaj datę urodzenia");
+        String data = scanner.next();
+
+        Student student = new Student(null, imie, nazwisko, indeks, data);
+        studentDao.dodajStudenta(student);
+    }
+
+    public void dzialaj() {
+        String tekst;
+        do {
+            System.out.println("Wybierz komendę: dodaj/usun/lista/edytuj/quit");
+            tekst = scanner.next();
+
+
+            if (tekst.equalsIgnoreCase("dodaj")) {
+                obsluzDodaj();
+            } else if (tekst.equalsIgnoreCase("usun")) {
+                obsluzUsun();
+            } else if (tekst.equalsIgnoreCase("lista")) {
+                obsluzLista();
+            } else if (tekst.equalsIgnoreCase("edytuj")) {
+                obsluzEdytuj();
+            }
+        } while (!tekst.equalsIgnoreCase("quit"));
+    }
+}
 
